b997a525f0729966981caedaf3d80ba09c829aba
Guest Engineer <guestengineer@guests-air.lan>
2025-03-18 10:59:59 -0700

    fixed user message bubble and copy btn

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,14 +18,16 @@ const Message: React.FC<MessageProps> = ({ text, isUser }) => {
    }
 
    return (
-      <div className={`p-3 rounded-3xl
-         ${isUser ? 'ml-auto' : 'mr-auto'}
-         ${isUser ? 'max-w-[30%]' : "max-w-[100%]"}
-         ${isUser ? 'bg-gray-200 dark:bg-slate-700 dark:border-gray-400 dark:text-gray-100 text-black whitespace-pre-wrap self-end' : 'self-start'}`
-      }>
-      {text}
-         <div>
-            <button onClick={copyToClipboard}>
+      <div>
+         <div className={`rounded-3xl
+            ${isUser ? 'ml-auto' : 'mr-auto'}
+            ${isUser ? 'w-fit' : "max-w-[100%]"}
+            ${isUser ? 'py-2 px-4 bg-gray-200 dark:bg-slate-700 dark:border-gray-400 dark:text-gray-100 text-black whitespace-pre-wrap self-end rounded-br-lg break-words' : 'self-start'}`
+         }>
+         {text}
+         </div>
+         <div className={`flex ${isUser ? 'justify-end' : 'justify-start'}`}>
+            <button onClick={copyToClipboard} className='hover:bg-gray-200 hover:rounded-full p-1.5 mt-1' title='Copy'>
                {isCopied ?
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
                      <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
